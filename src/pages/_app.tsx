95bab4b7f1a51f82ac6133b2efaea0b2041d7fc9
caseychu <62545079+CaseyChu960617@users.noreply.github.com>
2024-06-10 15:16:51 +0800

    feat: add husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { NextPageWithLayout } from "@/pages/page";
 import "@/styles/global.css";
 
-
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout;
 }
